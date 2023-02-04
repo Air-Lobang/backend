835c9b1271fd751b488355e9d9b6629381cfc4cf
Ryan Low <ryanlwk99@gmail.com>
2023-02-04 22:31:01 +0800

    Initialization

---
+++ src/main/java/com/example/demo/AirLobangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLobangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirLobangApplication.class, args);
+	}
+
+}
